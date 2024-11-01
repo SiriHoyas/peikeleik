7a2a7f0662c4b37992497e14b32c0a9a3879dcee
Siri Høyås <siri@skjervold.me>
2024-11-01 23:42:31 +0100

    Updates

--- assets/browser-Dt0ecsut.js
+++ assets/browser-Dt0ecsut.js
@@ -1,4 +1,4 @@
-import{c as Pe,g as Xe}from"./index-Bqjp7YCI.js";var se={exports:{}};/** @license
+import{c as Pe,g as Xe}from"./index-B4SoVFKF.js";var se={exports:{}};/** @license
  * eventsource.js
  * Available under MIT License (MIT)
  * https://github.com/Yaffle/EventSource/
